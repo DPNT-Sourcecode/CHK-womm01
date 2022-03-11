e0ce945818bc2e8288a23aeda61cecc8160166be
sbx_user1051 <sbx_user1051@169.254.20.21>
2022-03-11 20:05:03 +0000

    Fri Mar 11 20:05:03 UTC 2022

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -4,7 +4,10 @@ from solutions.CHK.checkout_solution import checkout
 
 
 class TestChk:
-    @pytest.mark.parametrize('skus, result', [['A', 50], ['B', 30], ['C', 20], ['D', 15], ['X', -1]])
+    @pytest.mark.parametrize('skus, result', [
+        ['A', 50], ['B', 30], ['C', 20], ['D', 15], ['X', -1], ["", -1], ["AA", -1], ["ABCD", -1]
+    ])
     def test_checkout(self, skus, result):
         assert result == checkout(skus)
+
 
