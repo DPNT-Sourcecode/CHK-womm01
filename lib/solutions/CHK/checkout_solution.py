3b964e44210276508b19ddf9af1f259e73ae1274
sbx_user1051 <sbx_user1051@169.254.20.233>
2022-03-11 21:48:05 +0000

    Fri Mar 11 21:48:05 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -11,6 +11,11 @@ def checkout(skus):
     offer_b = 0
     offer_e = 0
     offer_f = 0
+    offer_h = 0
+    offer_k = 0
+    offer_p = 0
+    offer_q = 0
+    offer_v = 0
     offer_eb = 0
     for sku in skus:
         if sku == 'A':
@@ -35,6 +40,47 @@ def checkout(skus):
             offer_f = offer_f + 1
             if offer_f == 2:
                 offer_f = -1
+        elif sku == 'G':
+            total = total + 20
+        elif sku == 'H':
+            total = total + 10
+            offer_h = offer_h + 1
+        elif sku == 'I':
+            total = total + 35
+        elif sku == 'J':
+            total = total + 60
+        elif sku == 'K':
+            total = total + 80
+        elif sku == 'L':
+            total = total + 90
+        elif sku == 'M':
+            total = total + 15
+        elif sku == 'N':
+            total = total + 40
+        elif sku == 'O':
+            total = total + 10
+        elif sku == 'P':
+            total = total + 50
+        elif sku == 'Q':
+            total = total + 30
+        elif sku == 'R':
+            total = total + 50
+        elif sku == 'S':
+            total = total + 30
+        elif sku == 'T':
+            total = total + 20
+        elif sku == 'U':
+            total = total + 40
+        elif sku == 'V':
+            total = total + 50
+        elif sku == 'W':
+            total = total + 20
+        elif sku == 'X':
+            total = total + 90
+        elif sku == 'Y':
+            total = total + 10
+        elif sku == 'Z':
+            total = total + 50
         else:
             return -1
     if offer_a > 4:
@@ -43,6 +89,12 @@ def checkout(skus):
         offer_a = offer_a % 5
     if offer_a > 2:
         total = total - 20
+    if offer_h > 9:
+        tmp = math.floor(offer_h / 10)
+        total = total - (20 * tmp)
+        offer_h = offer_h % 10
+    if offer_h > 4:
+        total = total - 5
     if offer_eb > 0 and offer_b > 0:
         if offer_b > offer_eb:
             offer_b = offer_b - offer_eb
@@ -53,4 +105,5 @@ def checkout(skus):
     if offer_b > 1:
         total = total - (15 * math.floor(offer_b / 2))
     return int(total)
+
 
--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -7,7 +7,8 @@ class TestChk:
     @pytest.mark.parametrize('skus, result', [
         ['A', 50], ['B', 30], ['C', 20], ['D', 15], ['F'*4, 30], ['F'*6, 40], ['EEEEBB', 160], ['EE', 80], ['A'*6, 250],
         ["", 0], ["A"*8, 330], ["ABCD", 115], ["AxA", -1], ["AAA", 130], ['BBBB', 90], ['BEBEEE', 160],
-        ['ABCDECBAABCABBAAAEEAA', 665], ['ABCDEABCDE', 280]
+        ['ABCDECBAABCABBAAAEEAA', 665], ['ABCDEABCDE', 280], ['H'*11, 90], ['H'*16, 135]
     ])
     def test_checkout(self, skus, result):
         assert checkout(skus) == result
+
