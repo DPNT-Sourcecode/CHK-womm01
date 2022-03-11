4f41f478b8adebc68d849ecc550be39eca18e312
sbx_user1051 <sbx_user1051@169.254.20.233>
2022-03-11 20:28:07 +0000

    Fri Mar 11 20:28:07 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -4,15 +4,21 @@
 # skus = unicode string
 def checkout(skus):
     total = 0
-    if skus == 'AAA':
-        return 130
-    if skus == 'BB':
-        return 45
+    offer_a = 0
+    offer_b = 0
     for sku in skus:
         if sku == 'A':
             total = total + 50
+            offer_a = offer_a + 1
+            if offer_a == 3:
+                offer_a = 0
+                total = total - 20
         elif sku == 'B':
             total = total + 30
+            offer_b = offer_b + 1
+            if offer_b == 2:
+                offer_b = 0
+                total = total - 5
         elif sku == 'C':
             total = total + 20
         elif sku == 'D':
@@ -20,6 +26,7 @@ def checkout(skus):
         else:
             return -1
     return total
+
 
 
 
--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -5,10 +5,11 @@ from solutions.CHK.checkout_solution import checkout
 
 class TestChk:
     @pytest.mark.parametrize('skus, result', [
-        ['A', 50], ['B', 30], ['C', 20], ['D', 15], ['AAAAAA', -1], ["", 0], ["AA", 100], ["ABCD", 115], ["AxA", -1], ["AAA", 130]
+        ['A', 50], ['B', 30], ['C', 20], ['D', 15], ['AAAAAA', 260], ["", 0], ["AA", 100], ["ABCD", 115], ["AxA", -1], ["AAA", 130]
     ])
     def test_checkout(self, skus, result):
         assert result == checkout(skus)
+
 
 
 
