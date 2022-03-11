61b6113d55080ebdb6906f5f5a10070d21d4868d
sbx_user1051 <sbx_user1051@169.254.20.233>
2022-03-11 21:10:06 +0000

    Fri Mar 11 21:10:06 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -34,12 +34,13 @@ def checkout(skus):
     if offer_a > 4:
         tmp = offer_a / 5
         total = total - (50 * tmp)
-        offer_a = offer_a - (tmp * 5)
+        offer_a = offer_a % 5
     if offer_a > 2:
         total = total - 20
     if offer_eb > 0:
         total = total - 30
-    return total
+    return int(total)
+
 
 
 
